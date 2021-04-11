e11cbcc4534a9a4c8f02187e233f4a8500ac9283
Prokopetz <nicolas@prokopetz.com.br>
2021-04-11 17:10:16 -0300

    add details, sort and filter

---
+++ src/script/index.js
@@ -0,0 +1,47 @@
+const list = document.getElementById('items');
+let items = [];
+let isSorted = false;
+for (let i = 5; i > 0; i--) {
+  const listItem = new ListItem(`${i}Exterminador do futuro`, '5.0', 'Longa metragem', 'Ação', '1984', 'Canal+', 'James Cameron');
+  listItem.addEventListener('click', () => onListItemClick(i));
+  items.push(listItem);
+  list.appendChild(listItem);
+}
+let filteredItems = [...items];
+
+
+const onListItemClick = (name) => {
+  window.location.href = `./details.html?movie=${name}`;
+}
+
+const onSortButtonClick = () => {
+  list.innerHTML = '';
+  filteredItems.sort((a, b) => {
+    if (a.name > b.name) {
+      return isSorted ? -1 : 1;
+    }
+    if (a.name < b.name) {
+      return isSorted ? 1 : -1;
+    }
+    return 0;
+  })
+  isSorted = !isSorted;
+  appendItemsToList();
+}
+
+const onFilterChange = (value) => {
+  list.innerHTML = '';
+  filteredItems = items.filter((item) => {
+    const movieName = item.name.toLowerCase();
+    const searchParam = value.toLowerCase();
+
+    return movieName.includes(searchParam);
+  })
+  appendItemsToList();
+}
+
+const appendItemsToList = () => {
+  filteredItems.forEach((item) => {
+    list.appendChild(item);
+  })
+}
